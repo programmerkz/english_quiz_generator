de1219bf62678cd0af002762a0387a61df04e3b8
Alexander <sanya_666@mail.ru>
2021-03-21 11:15:04 +0600

    Added PrasalVerb and Verb classes

---
+++ english_quiz_generator/PhrasalVerb.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace english_quiz_generator
+{
+    class PhrasalVerb
+    {
+        public Verb Verb { get; }
+        public string Preposition { get; }
+        public PhrasalVerb(Verb verb, string preposition)
+        {
+            if (Verb == null)
+                throw new ArgumentNullException("Argument verb is null.");
+
+            if (preposition == null)
+                throw new ArgumentNullException("Argument preposition is null.");
+
+            if (preposition == "")
+                throw new ArgumentException("Argument preposition is empty.");
+
+            Verb = verb;
+            Preposition = preposition;
+        }
+        public PhrasalVerb(string verb, string preposition)
+        {
+            if (verb == null)
+                throw new ArgumentNullException("Argument verb is null.");
+
+            if (verb == "")
+                throw new ArgumentException("Argument verb is empty.");
+
+            if (preposition == null)
+                throw new ArgumentNullException("Argument preposition is null.");
+
+            if (preposition == "")
+                throw new ArgumentException("Argument preposition is empty.");
+
+            Verb = new Verb(verb);
+            Preposition = preposition;
+        }
+    }
+}
---
+++ english_quiz_generator/Verb.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace english_quiz_generator
+{
+    class Verb
+    {
+        public string Infinitive { get; }
+        public string PastSimple { get; set; }
+        public string PastParticiple { get; set; }
+        public Verb(string infinitive_form)
+        {
+            if (infinitive_form == null)
+                throw new ArgumentNullException("Argument infinitive_form is null.");
+
+            if (infinitive_form == "")
+                throw new ArgumentException("Argument infinitive_form is empty.");
+
+            Infinitive = infinitive_form;
+        }
+    }
+}
