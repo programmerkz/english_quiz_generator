6155ccadd27a329d9914e7572f39bf68c855f988
Alexander <sanya_666@mail.ru>
2021-03-21 10:54:15 +0600

    Add project files.

---
+++ english_quiz_generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace english_quiz_generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
